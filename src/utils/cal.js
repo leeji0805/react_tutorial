e9c05d40f650fdb6b160e2995a5ce856fe9f1c3c
joshua <bomnaeum104@naver.com>
2024-03-25 15:17:22 +0900

    Revert "[add] #16 Jest - cal - minus, add 구현"
    This reverts commit 50d9267ae384d826d14f3dd29b3705cd9514e2d0.

--- src/utils/cal.js
+++ src/utils/cal.js
@@ -5,12 +5,3 @@
  * @returns 
  */
 export const add = (a, b) => a + b;
-/**
-* a - b 를 리턴한다 
-* @param {*} a 
-* @param {*} b 
-* @returns 
-*/
-export const minus = (a, b) => a - b;
-
-export const sumArray = (arr) => arr.reduce((acc, curr) => acc + curr, 0);
--- src/utils/cal.test.js
+++ src/utils/cal.test.js
@@ -1,6 +1,5 @@
 import { add } from './cal';
-import { minus } from './cal';
-import { sumArray } from './cal';
+
 // 단일 테스트 케이스
 test('adds 1 + 2 to equal 3', () => {
   const result = add(1, 2);
@@ -20,47 +19,6 @@ describe('add function tests', () => {
   test.each(cases)(
     'adds %p + %p to equal %p', (a, b, expectedResult) => {
       expect(add(a, b)).toBe(expectedResult);
-    }
-  );
-});
-
-
-// minus 함수에 대한 테스트
-describe('minus function tests', () => {
-  test('subtracts 5 - 2 to equal 3', () => {
-    expect(minus(5, 2)).toBe(3);
-  });
-
-  // 여러 테스트 케이스를 포함하는 경우
-  const cases = [
-    [5, 2, 3],
-    [10, 3, 7],
-    [4, 4, 0]
-  ];
-
-  test.each(cases)(
-    'subtracts %p - %p to equal %p', (a, b, expectedResult) => {
-      expect(minus(a, b)).toBe(expectedResult);
-    }
-  );
-});
-
-// sumArray 함수에 대한 테스트
-describe('sumArray function tests', () => {
-  test('sums [1, 2, 3, 4, 5] to equal 15', () => {
-    expect(sumArray([1, 2, 3, 4, 5])).toBe(15);
-  });
-
-  // 여러 배열에 대한 테스트 케이스
-  const cases = [
-    [[1, 2, 3], 6],
-    [[-1, 1], 0],
-    [[0, 0, 0, 1], 1]
-  ];
-
-  test.each(cases)(
-    'sums %p to equal %p', (arr, expectedResult) => {
-      expect(sumArray(arr)).toBe(expectedResult);
     }
   );
 });
