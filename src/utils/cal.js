50d9267ae384d826d14f3dd29b3705cd9514e2d0
joshua <bomnaeum104@naver.com>
2024-03-25 15:14:57 +0900

    [add] #16 Jest - cal - minus, add 구현

--- src/utils/cal.js
+++ src/utils/cal.js
@@ -5,3 +5,12 @@
  * @returns 
  */
 export const add = (a, b) => a + b;
+/**
+* a - b 를 리턴한다 
+* @param {*} a 
+* @param {*} b 
+* @returns 
+*/
+export const minus = (a, b) => a - b;
+
+export const sumArray = (arr) => arr.reduce((acc, curr) => acc + curr, 0);
